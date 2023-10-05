0aad1e0d3cae319224ed8ecb25f7892b3534042a
bharan <bharansooraj@gmail.com>
2023-10-05 10:44:04 +0530

    Initial commit

---
+++ elliptical/build/CMakeFiles/elliptical.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/elliptical.dir/elliptical.cc.o"
+  "CMakeFiles/elliptical.dir/elliptical.cc.o.d"
+  "CMakeFiles/elliptical.dir/main.cc.o"
+  "CMakeFiles/elliptical.dir/main.cc.o.d"
+  "elliptical"
+  "elliptical.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/elliptical.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ elliptical/build/CMakeFiles/elliptical.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for elliptical.
+# This may be replaced when dependencies are built.
